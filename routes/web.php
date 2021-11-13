7a37993d0d7991559b3d41f2ef35bfaf30db6729
ryuheihosei <ryuhei.basketball0718@softbank.ne.jp>
2021-11-13 12:36:21 +0900

    カリキュラム08-4

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', 'PostController@index');
 
 
 Route::get('/posts', 'PostController@index');
-
+Route::get('/posts/create', 'PostController@create');
+Route::post('/posts', 'PostController@store');
 
 Route::get('/posts/{post}', 'PostController@show');
