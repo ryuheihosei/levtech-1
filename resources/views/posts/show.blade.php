e1d4693fc6377aa757db4a09a9909517782163fb
ryuheihosei <ryuhei.basketball0718@softbank.ne.jp>
2021-11-14 12:32:14 +0900

    カリキュラム08-6

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,6 +11,12 @@
     <body>
         <h1>Blog Name</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{$post->id}}" id="form_delete" method="post">
+            @csrf
+            @method('DELETE')
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+        </form>
         <h1 class="title">
             {{ $post->title }}
         </h1>
@@ -25,5 +31,13 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e){
+                'use strict';
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
